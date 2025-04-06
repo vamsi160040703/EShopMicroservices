dce656702d8b9214e7f4f1c1b53bef606e7acf71
VAMSI KRISHNA <pellurivamsi@hotmail.com>
2025-04-06 13:52:59 +0530

    Implemention of  CatalogAPI

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProducts/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+using System.Windows.Input;
+
+namespace Catalog.API.Products.CreateProducts
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Create Product entity from command object
+            var product = new Product()
+            {
+                Name = command.Name,
+                Category=command.Category,
+                Description=command.Description,
+                ImageFile=command.ImageFile,
+                Price=command.Price
+            };
+            //ToDO
+            //save to DB
+            //Result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 //Add services to the container
 
 var app = builder.Build();
 
-//Configure teh HTTp request pipeline
 
+//Configure teh HTTp request pipeline
+app.MapCarter();
 app.Run();
