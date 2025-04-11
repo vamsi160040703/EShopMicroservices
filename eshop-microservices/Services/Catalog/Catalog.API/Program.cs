8f7eb70e740bc99f19d1c2a45cf37aecb7fe4fd7
VAMSI KRISHNA <pellurivamsi@hotmail.com>
2025-04-11 11:40:52 +0530

    Fluent validation in catalog api

--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,18 @@
 
 
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+
 });
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
 
 builder.Services.AddMarten(Opts =>
 {
