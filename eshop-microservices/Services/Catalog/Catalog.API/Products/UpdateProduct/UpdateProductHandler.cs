4f33df106bcc72b348136c0a1453c8127cf4a670
VAMSI KRISHNA <pellurivamsi@hotmail.com>
2025-04-11 17:33:08 +0530

    Custom Exceptions in Building Blocks for Catalog API

--- eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -4,31 +4,45 @@ namespace Catalog.API.Products.UpdateProduct;
 public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
-
-internal class UpdateProductCommandHandler
-    (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
-    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
 {
-    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    public UpdateProductCommandValidator()
     {
-        logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+        RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
 
-        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+        RuleFor(command => command.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
 
-        if (product is null)
-        {
-            throw new ProductNotFoundException();
-        }
-
-        product.Name = command.Name;
-        product.Category = command.Category;
-        product.Description = command.Description;
-        product.ImageFile = command.ImageFile;
-        product.Price = command.Price;
-
-        session.Update(product);
-        await session.SaveChangesAsync(cancellationToken);
-
-        return new UpdateProductResult(true);
+        RuleFor(command => command.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0");
     }
+}
+internal class UpdateProductCommandHandler
+        (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException(command.Id);
+            }
+
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    
 }
