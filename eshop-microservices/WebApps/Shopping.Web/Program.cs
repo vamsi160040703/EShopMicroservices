499178dacf1def4c7febb275691b265f781d4ec8
VAMSI KRISHNA <pellurivamsi@hotmail.com>
2025-05-19 10:55:46 +0530

    Completed the Ui Part

--- eshop-microservices/WebApps/Shopping.Web/Program.cs
+++ eshop-microservices/WebApps/Shopping.Web/Program.cs
@@ -18,6 +18,12 @@ builder.Services.AddRefitClient<IBasketService>()
         c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
     });
 
+builder.Services.AddRefitClient<IOrderingService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
